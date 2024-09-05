19bc58efbde9a78a1cc79fac6c103f4c1adb4f19
Atarook <ahmedtawad2002@gmail.com>
2024-09-05 21:09:09 +0300

    Modify Iam role for access and adding pipeline

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -93,7 +93,16 @@ resource "aws_iam_role_policy_attachment" "eks_node_group_cni" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"  # EKS CNI Policy
   role       = aws_iam_role.eks_node_group.name  # Attach to the Node Group role
 }
-
+# Attach the Amazon EKS CNI Policy to the IAM role
+resource "aws_iam_role_policy_attachment" "eks_node_group_ContainerRegistryFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"  # EKS CNI Policy
+  role       = aws_iam_role.eks_node_group.name  # Attach to the Node Group role
+}
+# Attach the Amazon EKS CNI Policy to the IAM role
+resource "aws_iam_role_policy_attachment" "eks_node_group_FullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"  # EKS CNI Policy
+  role       = aws_iam_role.eks_node_group.name  # Attach to the Node Group role
+}
 # IAM Role for Master Access to EKS
 resource "aws_iam_role" "master_access_team_4" {
   name               = "team4-masterPermissionRole"  # Name of the IAM role for master access
@@ -101,9 +110,76 @@ resource "aws_iam_role" "master_access_team_4" {
   tags               = var.tags  # Tags for the IAM role
 }
 
-# Attach a custom policy for master access to the IAM role
-resource "aws_iam_role_policy" "master_access_policy" {
-  name   = "team4-masterAccessPolicy"  # Name of the IAM role policy
-  role   = aws_iam_role.master_access_team_4.id  # Attach to the master access role
-  policy = data.aws_iam_policy_document.eks_full_access.json  # The policy document defining full access permissions
+
+
+resource "aws_iam_policy" "ec2_permissions_policy" {
+  name        = "EC2PermissionsForNodeGroup"
+  description = "EC2 permissions to attach and detach volumes, describe volumes, and more."
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:AttachVolume",
+          "ec2:DetachVolume",
+          "ec2:DescribeInstances",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumeStatus",
+          "ec2:DescribeTags",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeVolumesModifications",
+          "ec2:ModifyVolume",
+          "ec2:CreateVolume",
+          "ec2:CreateTags"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_ec2_permissions" {
+  policy_arn = aws_iam_policy.ec2_permissions_policy.arn  # Use the EC2 policy you just created
+  role       = aws_iam_role.eks_node_group.name           # Attach to the Node Group role
+}
+
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = jsonencode([{
+      rolearn  = aws_iam_role.eks_node_group.arn
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups   = ["system:bootstrappers", "system:nodes"]
+    }])
+
+    mapUsers = jsonencode([{
+      userarn  = "arn:aws:iam::637423483309:user/fawzy"
+      username = "fawzy"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::637423483309:user/ahmedkamal"
+      username = "ahmedkamal"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::637423483309:user/Tarek"
+      username = "Tarek"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::637423483309:user/ahmed"
+      username = "ahmed"
+      groups   = ["system:masters"]
+    }
+    
+    ])
+  }
 }
